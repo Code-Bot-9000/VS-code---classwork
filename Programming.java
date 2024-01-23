86161048dbbd0e5ca971ee6738005a32919a1f83
Code-Bot-9000 <berniepc@icloud.com>
2024-01-22 21:36:09 -0600

    java programming

---
+++ Programming.java
@@ -0,0 +1,9 @@
+public class Programming
+{
+   public static void main(String args[])
+   {
+      // Write your code here
+      System.out.println("I'm learning how to program in Java.");
+      System.out.println("That's awesome!");
+   }
+}
