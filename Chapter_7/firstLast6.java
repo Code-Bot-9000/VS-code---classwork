6d635cf15d3c4addedaed96eff7d887643c9e483
Code-Bot-9000 <berniepc@icloud.com>
2024-04-03 21:34:44 -0500

    updated

---
+++ Chapter_7/firstLast6.java
@@ -0,0 +1,10 @@
+package Chapter_7;
+
+public class firstLast6 {
+    public static void main(String[] args) {
+        int nums[];
+        nums[0] == 6 || nums[nums.length-1] == 6;
+    }
+
+    
+}
