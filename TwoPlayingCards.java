af4eaef469294ab5e47b57d9656840bbd38352d1
Code-Bot-9000 <berniepc@icloud.com>
2024-05-04 21:48:37 -0500

    updated

--- TwoPlayingCards.java
+++ TwoPlayingCards.java
@@ -2,12 +2,15 @@
 public class TwoPlayingCards{
     public static void main(String[] args){
         String suits[] = {"Hearts","Diamonds","Clubs","Spades"};
-        int random1 = (int)(Math.random() * 12) + 1;
-        int random2 = (int)(Math.random() * 12) + 1;
+        int suitType1 = (int)(Math.random() * 4);
+        int suitType2 = (int)(Math.random() * 4);
+        int cardValue1 = (int)(Math.random() * 13) + 1;
+        int cardValue2 = (int)(Math.random() * 13) + 1;
         
-        Card card1 = new Card(suits[random1]);
-        Card card2 = new Card();
+        Card card1 = new Card(suits[suitType1],cardValue1);
+        Card card2 = new Card(suits[suitType2],cardValue2);
 
-        System.out.println(random);
+        System.out.println("Playing card 1: "+ card1.toString());
+        System.out.println("Playing card 2: "+ card2.toString());
     }
 }
